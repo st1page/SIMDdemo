4a0b16617ab4e989930aa909ff3bf75facb04f8b
st1page <1245835950@qq.com>
2021-05-25 02:06:25 -0400

    add avx512

--- memcpy/memcpy.hpp
+++ memcpy/memcpy.hpp
@@ -6,7 +6,7 @@
 #include <immintrin.h>
 
 inline void mm256Memcpy(char* dst, const char* src, const size_t len);
-//inline void mm512Memcpy(char* dst, const char* src, const size_t len);
+inline void mm512Memcpy(char* dst, const char* src, const size_t len);
 inline void longMemcpy(char* dst, const char* src, const size_t len);
 inline void naiveMemcpy(char* dst, const char* src, const size_t len);
 
@@ -25,6 +25,11 @@ inline void Memcpy(char* dst, const char* src, const size_t len) {
     mm256Memcpy(dst, src, len);
     return;
 #endif
+#ifdef AVX512_MEMCPY
+    mm512Memcpy(dst, src, len);
+    return;
+#endif
+
 #ifdef STD_MEMCPY
     memcpy(dst,src,len);
     return;
@@ -60,6 +65,20 @@ inline void mm256Memcpy(char* dst, const char* src, const size_t len) {
 	for(int i=0;i<task_num;i++) {
         register const __m256i tmp = _mm256_loadu_si256((const __m256i *)src);
         _mm256_storeu_si256((__m256i *)dst, tmp);
+        src += step;
+        dst += step;
+    }
+    longMemcpy(dst, src, remain_num);
+    return;
+}
+
+inline void mm512Memcpy(char* dst, const char* src, const size_t len) {
+    size_t step = 512/8; 
+    size_t task_num = len / step;
+    size_t remain_num = len - task_num * step;
+	for(int i=0;i<task_num;i++) {
+        register const __m512i tmp = _mm512_loadu_si512((const __m512i *)src);
+        _mm512_storeu_si512((__m512i *)dst, tmp);
         src += step;
         dst += step;
     }
