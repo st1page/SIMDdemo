7e715d782fe8eb4533fcff5ca74ce2b08fc00a86
st1page <1245835950@qq.com>
2021-05-25 01:28:53 -0400

    test bash

---
+++ memcpy/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#set -x
+set -e
+set -u
+make clean
+make
+
+for exec_file in `ls test_*`;
+do
+    echo "running $exec_file"
+    ./$exec_file | tee $exec_file.result
+    perf stat -e cache-misses ./$exec_file | tee -a $exec_file.result
+done
